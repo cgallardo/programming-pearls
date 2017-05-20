802ae7f1ab29179dd240e32f4a6d9e474849e795
Carlos Gallardo <arganboy@gmail.com>
2017-05-20 23:19:58 +0200

    Create functions

--- src/column1/solution.php
+++ src/column1/solution.php
@@ -1,24 +1,30 @@
 <?php
 
-echo "Running...";
+function loadNumbers($inputFileName, &$numbers)
+{
+    $inputFile = new SplFileObject($inputFileName, 'r');
+
+    while (!$inputFile->eof()) {
+        $key = (int) $inputFile->fgets();
+        $numbers[$key] = 1;
+    }
+
+    unset($inputFile);
+}
+
+function writeNumbers($outputFileName, $numbers)
+{
+    $outputFile = new SplFileObject($outputFileName, 'w');
+
+    foreach ($numbers as $key => $value) {
+        if (1 === $value) {
+            $outputFile->fwrite(strval($key) . PHP_EOL);
+        }
+    }
+
+    unset($outputFile);
+}
 
 $numbers = array_fill(0, 10000000, 0);
-$inputFile = new SplFileObject('input.txt', 'r');
-
-while (!$inputFile->eof()) {
-    $key = (int) $inputFile->fgets();
-    $numbers[$key] = 1;
-}
-
-unset($inputFile);
-
-$outputFile = new SplFileObject('output.txt', 'w');
-foreach ($numbers as $key => $value) {
-    if (1 === $value) {
-        $outputFile->fwrite(strval($key) . PHP_EOL);
-    }
-}
-
-unset($outputFile);
-
-echo "\nFinished";
+loadNumbers('input.txt', $numbers);
+writeNumbers('output.txt', $numbers);
